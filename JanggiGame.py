a035c4001ff58e8d8a15823d4199424ba1e9ebb4
Brandon Lenz <71602449+Vikesfoeva@users.noreply.github.com>
2021-03-04 21:55:28 -0500

    Cleaned a test

--- JanggiGame.py
+++ JanggiGame.py
@@ -631,19 +631,19 @@ class General(GamePiece):
         return "Red General"
 
 # Basic Tests
-game = JanggiGame()
-#
-game.print_board()
-game.make_move("a7","a6")
-game.print_board()
-game.make_move("i4","i5")
-game.print_board()
-game.make_move("a6","a5")
-game.print_board()
-game.make_move("i5","i6")
-game.print_board()
-game.make_move("a5","a4")
-game.print_board()
+# game = JanggiGame()
+# #
+# game.print_board()
+# game.make_move("a7","a6")
+# game.print_board()
+# game.make_move("i4","i5")
+# game.print_board()
+# game.make_move("a6","a5")
+# game.print_board()
+# game.make_move("i5","i6")
+# game.print_board()
+# game.make_move("a5","a4")
+# game.print_board()
 
 # # Move a guard around
 # game.make_move("e9", "e8")
